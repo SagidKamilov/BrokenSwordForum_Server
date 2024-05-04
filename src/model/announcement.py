5ab8a31589d51d894dea8753defe4af7c9c1c58d
SagidKamilov <89615854074a@gmail.com>
2024-05-04 19:06:02 +0300

    Добавил хранени даты создания и обновления

--- src/model/announcement.py
+++ src/model/announcement.py
@@ -1,5 +1,4 @@
-from typing import List
-from datetime import datetime, date
+from datetime import datetime
 
 import sqlalchemy
 from sqlalchemy.orm import Mapped, mapped_column, relationship
@@ -9,10 +8,13 @@ from src.model.base import Base
 
 
 class Announcement(Base):
-    __tablename__ = "announcement"
+    __tablename__ = "announcement_bs"
 
     id: Mapped[int] = mapped_column(name="announcement_id", primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(name="name", type_=sqlalchemy.String(100), unique=True, nullable=False)
-    description: Mapped[str] = mapped_column(name="description", type_=sqlalchemy.Text(), nullable=False)
-    user_id: Mapped[int] = mapped_column(sqlalchemy.ForeignKey("user.user_id"))
+    description: Mapped[str] = mapped_column(name="description", type_=sqlalchemy.Text, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(name="created_at", type_=sqlalchemy.DateTime(timezone=True), server_default=functions.now())
+    update_at: Mapped[datetime] = mapped_column(name="update_at", type_=sqlalchemy.DateTime(timezone=True),
+                                                server_default=functions.now(), onupdate=datetime.now())
+    user_id: Mapped[int] = mapped_column(sqlalchemy.ForeignKey("user_bs.user_id", ondelete="CASCADE"))
     user: Mapped["User"] = relationship(back_populates="announcements")
--- src/model/new.py
+++ src/model/new.py
@@ -1,5 +1,4 @@
-from typing import List
-from datetime import datetime, date
+from datetime import datetime
 
 import sqlalchemy
 from sqlalchemy.orm import Mapped, mapped_column, relationship
@@ -9,11 +8,14 @@ from src.model.base import Base
 
 
 class New(Base):
-    __tablename__ = "new"
+    __tablename__ = "new_bs"
 
     id: Mapped[int] = mapped_column(name="new_id", primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(name="name", type_=sqlalchemy.String(100), unique=True, nullable=False)
-    description: Mapped[str] = mapped_column(name="description", type_=sqlalchemy.Text(), nullable=False)
-    picture: Mapped[str] = mapped_column(name="picture", type_=sqlalchemy.Text(), nullable=False)
-    user_id: Mapped[int] = mapped_column(sqlalchemy.ForeignKey("user.user_id"))
+    description: Mapped[str] = mapped_column(name="description", type_=sqlalchemy.Text, nullable=False)
+    picture: Mapped[str] = mapped_column(name="picture", type_=sqlalchemy.Text, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(name="created_at", type_=sqlalchemy.DateTime(timezone=True), server_default=functions.now())
+    update_at: Mapped[datetime] = mapped_column(name="update_at", type_=sqlalchemy.DateTime(timezone=True),
+                                                server_default=functions.now(), onupdate=datetime.now())
+    user_id: Mapped[int] = mapped_column(sqlalchemy.ForeignKey("user_bs.user_id", ondelete="CASCADE"))
     user: Mapped["User"] = relationship(back_populates="news")
