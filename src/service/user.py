41ff5e4e8ea7e74e8891dc82a28b45c9ab6bc42a
SagidKamilov <89615854074a@gmail.com>
2024-05-04 19:09:16 +0300

    Дополнил методами работы через репозиторий: CRUD + проверка существования записи по id и email

--- src/service/user.py
+++ src/service/user.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from src.repository.user import UserRepository
 from src.model.user import User
 from src.dto.user import UserCreate, UserUpdate, UserResponse
@@ -11,9 +13,16 @@ class UserService:
 
     async def create_user(self, user_create: UserCreate) -> UserResponse:
 
+        check_duplicate: bool = await self.check_duplicate(user_email=user_create.email)
+
+        if check_duplicate:
+            raise Exception(f"Email `{user_create.email}` занят!")
+
+        hashed_password: str = await self.hash_password(password=user_create.password)
+
         user_create_hash_password = UserCreate(
             username=user_create.username,
-            password=self.hash_generator.generate_hash_from_password(password=user_create.password),
+            password=hashed_password,
             email=user_create.email,
             birthday=user_create.birthday
         )
@@ -26,3 +35,91 @@ class UserService:
             email=user.email,
             birthday=user.birthday
         )
+
+    async def get_user(self, user_id: int) -> UserResponse:
+        user: User = await self.repository.get_user_by_id(user_id=user_id)
+
+        if not user:
+            raise Exception(f"Пользователь с id = `{user_id}` не был найден!")
+
+        return UserResponse(
+            id=user.id,
+            username=user.username,
+            email=user.email,
+            birthday=user.birthday
+        )
+
+    async def get_users(self) -> List[UserResponse]:
+        users = await self.repository.get_users()
+
+        users_response_list = [
+            UserResponse(
+                id=user.id,
+                username=user.username,
+                email=user.email,
+                birthday=user.birthday
+            )
+            for user
+            in users
+        ]
+
+        return users_response_list
+
+    async def update_user(self, user_id: int, user_update: UserUpdate) -> UserResponse:
+        check_user_exist: bool = await self.check_user_exist(user_id=user_id)
+
+        if not check_user_exist:
+            raise Exception(f"Пользователь с id = `{user_id}` не был найден!")
+
+        check_duplicate: bool = await self.check_duplicate(user_email=user_update.email)
+
+        if check_duplicate:
+            raise Exception(f"Email `{user_update.email}` занят!")
+
+        hashed_password: str = await self.hash_password(password=user_update.password)
+
+        user_update_hash_password = UserUpdate(
+            username=user_update.username,
+            password=hashed_password,
+            email=user_update.email,
+            birthday=user_update.birthday
+        )
+
+        user: User = await self.repository.update_user_by_id(user_id=user_id, user_update=user_update_hash_password)
+
+        return UserResponse(
+            id=user.id,
+            username=user.username,
+            email=user.email,
+            birthday=user.birthday
+        )
+
+    async def delete_user(self, user_id: int) -> str:
+        check_user_exist: bool = await self.check_user_exist(user_id=user_id)
+
+        if not check_user_exist:
+            raise Exception(f"Пользователь с id = `{user_id}` не был найден!")
+
+        result: str = await self.repository.delete_user_by_id(user_id=user_id)
+
+        return result
+
+    async def hash_password(self, password: str) -> str:
+        hashed_password = self.hash_generator.generate_hash_from_password(password=password)
+        return hashed_password
+
+    async def check_duplicate(self, user_email: str) -> bool:
+        user: User = await self.repository.get_user_by_email(user_email=user_email)
+
+        if user:
+            return True
+        else:
+            return False
+
+    async def check_user_exist(self, user_id: int) -> bool:
+        user: User = await self.repository.get_user_by_id(user_id=user_id)
+
+        if user:
+            return True
+        else:
+            return False
