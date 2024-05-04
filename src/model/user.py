6b514ee58ed2220bbce9f97650d3819c482f9caa
SagidKamilov <89615854074a@gmail.com>
2024-05-04 12:28:24 +0300

    Удалил вызов класса

--- src/model/user.py
+++ src/model/user.py
@@ -15,9 +15,9 @@ class User(Base):
     username: Mapped[str] = mapped_column(name="username", type_=sqlalchemy.String(255), nullable=False)
     birthday: Mapped[date] = mapped_column(name="birthday", type_=sqlalchemy.Date(), nullable=False)
     email: Mapped[str] = mapped_column(name="email", type_=sqlalchemy.String(100), unique=True, nullable=False)
-    hash_password: Mapped[str] = mapped_column(name="hash_password", type_=sqlalchemy.Text(), nullable=False)
+    hash_password: Mapped[str] = mapped_column(name="hash_password", type_=sqlalchemy.Text, nullable=False)
     created_at: Mapped[datetime] = mapped_column(name="created_at", type_=sqlalchemy.DateTime(timezone=True), server_default=functions.now())
     update_at: Mapped[datetime] = mapped_column(name="update_at", type_=sqlalchemy.DateTime(timezone=True),
                                                 server_default=functions.now(), onupdate=datetime.now())
-    announcements: Mapped[List["Announcement"]] = relationship(back_populates="user")
-    news: Mapped[List["New"]] = relationship(back_populates="user")
+    # announcements: Mapped[List["Announcement"]] = relationship(back_populates="user")
+    # news: Mapped[List["New"]] = relationship(back_populates="user")
